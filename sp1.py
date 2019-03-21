82685a584e3334674e2a7dcb1d7c738b947d025c
BhavaSuryaVamsiSrireddy <sbs.vamsi3@gmail.com>
2019-03-21 18:16:49 +0530

    initial commit

--- sp1.py
+++ sp1.py
@@ -1,7 +1,10 @@
-n=int(input())
+n=int(input('Input:'))
 if(n>0):
+    print('Output:')
     print('Positive')
 elif(n<0):
+    print('Output:')
     print('Negative')
 else:
+    print('Output:')
     print('Zero')
