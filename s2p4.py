91da0fa0fca80bbd99ecbaa4b7fa1e416f887193
BhavaSuryaVamsiSrireddy <43814473+BhavaSuryaVamsiSrireddy@users.noreply.github.com>
2019-03-22 19:34:36 +0530

    Update s2p4.py

--- s2p4.py
+++ s2p4.py
@@ -3,11 +3,15 @@ try:
     n=n.split()
     i=int(n[0])
     j=int(n[1])
+    l=[]
     if(i<=10000 and j<=10000):
         for k in range(i+1,j):
             if(k%2==0):
                 continue
             else:
-                print(k,end=' ')
+                l.append(k)
+        for i in range(len(l)-1):
+            print(l[i],end=' ')
+        print(l[-1])
 except:
     print('invalid')
