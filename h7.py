ac0ccde0f652c1b0af4bc0f017be6c6a499d1931
BhavaSuryaVamsiSrireddy <43814473+BhavaSuryaVamsiSrireddy@users.noreply.github.com>
2019-03-23 20:53:54 +0530

    Create h7.py

---
+++ h7.py
@@ -0,0 +1,22 @@
+try:
+    n=int(input())
+    m=input()
+    m=m.split()
+    l=[]
+    res=[]
+    for i in m:
+        l.append(int(i))
+    
+    for i in range(len(l)):
+        if(i%2==0):
+            if(l[i]%2!=0):
+                res.append(l[i])
+        else:
+            if(l[i]%2==0):
+                res.append(l[i])
+    for i in range(len(res)-1):
+        print(res[i],end=' ')
+    print(res[-1])
+        
+except:
+    print('invalid')
