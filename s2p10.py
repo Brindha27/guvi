7f2df65268ad201f564f9c99287667f3cb9adb8b
BhavaSuryaVamsiSrireddy <43814473+BhavaSuryaVamsiSrireddy@users.noreply.github.com>
2019-03-23 11:08:12 +0530

    Update s2p10.py

--- s2p10.py
+++ s2p10.py
@@ -2,7 +2,7 @@ try:
     n=int(input())
     r=[]
     for i in range(1,6):
-        r.append(i*5)
+        r.append(i*n)
     for i in range(len(r)-1):
         print(r[i],end=' ')
     print(r[-1])
