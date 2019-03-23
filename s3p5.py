8d4834724cf56cc43f564907f0df5b0197232134
BhavaSuryaVamsiSrireddy <43814473+BhavaSuryaVamsiSrireddy@users.noreply.github.com>
2019-03-23 16:25:06 +0530

    Create s3p5.py

---
+++ s3p5.py
@@ -0,0 +1,19 @@
+try:
+    s=int(input())
+    n=input()
+    n=n.split()
+    l=[]
+    for i in n:
+        t=int(i)
+        l.append(t)
+    l=sorted(l)
+    print(l)
+    if(s%2==0):
+        s=s//2
+        m=(l[s]+l[s-1])//2
+    else:
+        s=s//2
+        m=l[s]
+    print(m)
+except:
+    print('invalid')
