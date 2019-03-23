22467cd6ef591c4cdc28e227a8f3dc262faaeb61
BhavaSuryaVamsiSrireddy <43814473+BhavaSuryaVamsiSrireddy@users.noreply.github.com>
2019-03-23 10:15:55 +0530

    Update s2p3.py

--- s2p3.py
+++ s2p3.py
@@ -1,14 +1,16 @@
 try:
     n=int(input())
-    i=n//2
     f=0
-    for j in range(2,i):
-        if(n%j==0):
-            f=1
-            break
-    if(f==1):
-        print('no')
+    if (n>1):    
+        for j in range(2,n):
+            if(n%j==0):
+                f=1
+                break
+        if(f==1):
+            print('no')
+        else:
+            print('yes')
     else:
-        print('yes')   
+        print('no')
 except:
     print('invalid')
