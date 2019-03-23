7447a1c1900cdc999124fbb7585333daa5d5d6e8
BhavaSuryaVamsiSrireddy <43814473+BhavaSuryaVamsiSrireddy@users.noreply.github.com>
2019-03-23 11:05:13 +0530

    Create s2p10.py

---
+++ s2p10.py
@@ -0,0 +1,10 @@
+try:
+    n=int(input())
+    r=[]
+    for i in range(1,6):
+        r.append(i*5)
+    for i in range(len(r)-1):
+        print(r[i],end=' ')
+    print(r[-1])
+except:
+    print('invalid')
