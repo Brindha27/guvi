1c795696f635e4bb254a99cf22a6930bcc1b6aff
BhavaSuryaVamsiSrireddy <43814473+BhavaSuryaVamsiSrireddy@users.noreply.github.com>
2019-03-24 11:03:52 +0530

    Create h11.py

---
+++ h11.py
@@ -0,0 +1,13 @@
+try:
+    s=input()
+    s=s.split()
+    print(len(s))
+    res=[]
+    for i in s:
+        a=i[::-1]
+        res.append(a)
+    for i in range(len(res)-1):
+        print(res[i],end=' ')
+    print(res[-1])
+except:
+    print('invalid')
