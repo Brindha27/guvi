4714207f9bfde3fa09cf3eae9f399e3f69fcda40
BhavaSuryaVamsiSrireddy <43814473+BhavaSuryaVamsiSrireddy@users.noreply.github.com>
2019-04-10 11:29:18 +0530

    Update pl14.py

--- pl14.py
+++ pl14.py
@@ -1,8 +1,9 @@
 try:
+    le=int(input())
     n=input()
     l=['a','e','i','o','u','A','E','I','O','U']
     r=[]
-    for i in range(len(n)):
+    for i in range(le):
         if n[i] not in l:
             r.append(n[i])
     r=r[::-1]
