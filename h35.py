4b5716f1e2a86a0e7e8e7d73aa701f4366ce384b
BhavaSuryaVamsiSrireddy <43814473+BhavaSuryaVamsiSrireddy@users.noreply.github.com>
2019-04-11 19:41:12 +0530

    Update h35.py

--- h35.py
+++ h35.py
@@ -10,13 +10,10 @@ try:
         le=l//2
         nl.remove(nl[le])
         le=int(len(nl)/2)
-        c1,c2,f=[],[],0
+        c1,c2=[],[]
         c1=c1+nl[:le]
         c2=c2+nl[le:]
-        for i in range(len(c1)):
-            if(c1[i]==c2[i]):
-                f=f+1
-        if(f==len(c1)):
+        if (c1==c2):
             print('YES')
         else:
             print('NO')
